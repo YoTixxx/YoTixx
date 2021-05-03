e38c3a9d1dc4627c04279c9b5a0582c21087fe27
David Ghazi <davidaghazi@gmail.com>
2021-05-03 13:02:24 -0400

    completed main layout of sign up page

--- src/components/SignPage/index.js
+++ src/components/SignPage/index.js
@@ -1,9 +1,11 @@
 import SignIn from "../SignPage/SignIn/SignIn";
+import SignUp from "../SignPage/SignUp/SignUp";
 
 export default function SignPage() {
   return (
     <>
       <SignIn />
+      <SignUp />
     </>
   );
 }
