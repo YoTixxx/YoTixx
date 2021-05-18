9135837a973a5a6811fd0757c8a353b3659f034f
leeroy Phili <leeroy@myshoperon.com>
2021-05-18 16:07:28 -0400

    router setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import Signup from "./SignPage/SignUp/SignUp";
+import SignIn from "./SignPage/SignIn/SignIn";
+import Home from "./HomePage/Home"
 import { AuthProvider } from "../contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 
 function App() {
   return (
+    <>
       <Router>
-    <AuthProvider>
-      <Signup />
-    </AuthProvider>
-    </Router>
+        <AuthProvider>
+          <Switch>
+            <Route  exact path="/" component={Home}/>
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={SignIn} />
+
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </>
   );
 }
 
