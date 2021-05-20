5c65f552c749f7b3a4d18f878b4c4c2f79870bdf
leeroy Phili <leeroy@myshoperon.com>
2021-05-20 14:06:20 -0400

    logout logic

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -1,4 +1,7 @@
 import { makeStyles } from "@material-ui/core/styles";
+import  {useState} from "react"
+import {useAuth} from "../../contexts/AuthContext";
+import {Link, useHistory} from "react-router-dom"
 
 const useStyles = makeStyles((theme) => ({
   test: {
@@ -8,10 +11,26 @@ const useStyles = makeStyles((theme) => ({
 
 function Home() {
   const classes = useStyles();
+  const [error, setError] = useState("")
+  const {currentUser, logout} = useAuth()
+  const history= useHistory()
+
+  async function handleLogout(){
+    setError('')
+
+    try{
+      await logout()
+      history.push("/login")
+    } catch{
+      setError('Failed to log out')
+    }
+  }
+
   return (
     <div className={classes.test}>
       <h1>YoTixx</h1>
       <h1>Testing the automatic deploy again</h1>
+<button onClick={handleLogout}>Logout</button>
     </div>
   );
 }
