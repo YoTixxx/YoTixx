c49ddf7c5e3668d6f8fde37686173e1706b82b04
leeroy Phili <leeroy@myshoperon.com>
2021-05-27 16:04:02 -0400

    yotixx reusable header

--- src/components/ReusableComponents/HomeHeader/Index.js
+++ src/components/ReusableComponents/HomeHeader/Index.js
@@ -1,16 +1,26 @@
 import "./header.scss";
-import YoTixx from "../../../images/ReusableImages/YoTixx";
+import YoTixx from "../../../images/ReusableImages/YoTixx.svg";
 
-const HomeHeader = () => {
+const HomeHeader = ({searchValue, onchange, submit}) => {
   return (
-    <div>
-      <div>
+    <header className="Main-header">
+      <div className="Main-header__logo">
         <img src={YoTixx} alt="Yotixx logo" />
       </div>
-      <div>
-          
+      <div className="Main-header__form">
+          <form onSubmit={submit}>
+              <input
+              className="Main-header__input"
+              title="Search for Artist, teams, venue"
+              aria-label="Enter your search for Artist, teams, venue"
+              type="search"
+              value={searchValue}
+              placeholder={"Search for Artist, teams, venue"}
+              onChange={onchange}
+              />
+          </form>
       </div>
-    </div>
+    </header>
   );
 };
 
