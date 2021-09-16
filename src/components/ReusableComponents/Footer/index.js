8532bd5b6ba2bdabb88cf07ddf243c815fa00891
leeroy Phili <leeroy@myshoperon.com>
2021-09-16 17:54:41 -0400

    bottom footer layout

---
+++ src/components/ReusableComponents/Footer/index.js
@@ -0,0 +1,21 @@
+import "./Footer.scss";
+
+const Footer = () =>{
+    return(
+<section className="">
+    <footer className="footer">
+        <div className="footer__top"></div>
+        <div className="footer__bottom">
+            <p>YoTixx</p>
+            <p>© 2021 YoTixx. All rights reserved.</p>
+            <div>
+                
+            </div>
+        </div>
+
+    </footer>
+</section>
+    )
+}
+
+export default Footer
