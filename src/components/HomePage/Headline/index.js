92f3d51d95430838cc411427889f90309efcf5ce
David Ghazi <davidaghazi@gmail.com>
2021-09-22 01:47:38 -0400

    added info to the headline component

--- src/components/HomePage/Headline/index.js
+++ src/components/HomePage/Headline/index.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeadlineEvents from "../../ReusableComponents/HeadlineEvents/index";
 
 const HomeHeadline = () => {
+  const API_KEY = process.env.REACT_APP_TICKETMASTER_API_KEY;
+  let sportsURL = `https://app.ticketmaster.com/discovery/v2/events.json?classificationName=sports&countryCode=US&apikey=${API_KEY}&size=1`;
+
+  let [eventImage, setEventImage] = useState(null);
+  let [eventTitle, setEventTitle] = useState(null);
+  let [eventDescription, setEventDescription] = useState(null);
+
+  useEffect(() => {
+    fetch(sportsURL)
+      .then((response) => response.json())
+      .then((data) => {
+        setEventImage(data._embedded.events[0].images[3].url);
+        setEventTitle(data._embedded.events[0].name);
+        setEventDescription(
+          data._embedded.events[0].ticketing.healthCheck.description
+        );
+      });
+  }, []);
+
   return (
     <div>
-      <HeadlineEvents />
+      <HeadlineEvents
+        image={eventImage}
+        title={eventTitle}
+        description={eventDescription}
+        btnText="See Tickets"
+      />
     </div>
   );
 };
