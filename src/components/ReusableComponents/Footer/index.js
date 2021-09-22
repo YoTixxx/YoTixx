6951b409fa6540371fdec320559e1cdf71e896ac
leeroy Phili <leeroy@myshoperon.com>
2021-09-22 01:50:21 -0400

    styled components depennce

--- src/components/ReusableComponents/Footer/index.js
+++ src/components/ReusableComponents/Footer/index.js
@@ -1,14 +1,81 @@
 import "./Footer.scss";
-import twitter from "../../../images/twitter.png"
-import facebook from "../../../images/facebook.png"
-import instagram from "../../../images/instagram.png"
+import {
+    Box,
+    Container,
+    Row,
+    Column,
+    FooterLink,
+    Heading,
+  } from "./FooterStyles";
+
+import twitter from "../../../images/twitter2.png"
+import facebook from "../../../images/facebook3.png"
+import instagram from "../../../images/instagram2.png"
 
 
 const Footer = () =>{
     return(
-<section className="">
+<section className="main">
     <footer className="footer">
-        <div className="footer__top"></div>
+        <div className="footer__top">
+        <Container>
+        <Row>
+          <Column>
+            <Heading>About Us</Heading>
+            <FooterLink href="#">Aim</FooterLink>
+            <FooterLink href="#">Vision</FooterLink>
+            <FooterLink href="#">Testimonials</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Services</Heading>
+            <FooterLink href="#">Writing</FooterLink>
+            <FooterLink href="#">Internships</FooterLink>
+            <FooterLink href="#">Coding</FooterLink>
+            <FooterLink href="#">Teaching</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Contact Us</Heading>
+            <FooterLink href="#">Uttar Pradesh</FooterLink>
+            <FooterLink href="#">Ahemdabad</FooterLink>
+            <FooterLink href="#">Indore</FooterLink>
+            <FooterLink href="#">Mumbai</FooterLink>
+          </Column>
+          <Column>
+            <Heading>Social Media</Heading>
+            <FooterLink href="#">
+              <i className="fab fa-facebook-f">
+                <span style={{ marginLeft: "10px" }}>
+                  Facebook
+                </span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-instagram">
+                <span style={{ marginLeft: "10px" }}>
+                  Instagram
+                </span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-twitter">
+                <span style={{ marginLeft: "10px" }}>
+                  Twitter
+                </span>
+              </i>
+            </FooterLink>
+            <FooterLink href="#">
+              <i className="fab fa-youtube">
+                <span style={{ marginLeft: "10px" }}>
+                  Youtube
+                </span>
+              </i>
+            </FooterLink>
+          </Column>
+        </Row>
+      </Container>
+            
+        </div>
+        <br/>
         <div className="footer__bottom">
             <p>YoTixx</p>
             <p>© 2021 YoTixx. All rights reserved.</p>
