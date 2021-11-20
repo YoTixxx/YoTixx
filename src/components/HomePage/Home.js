3cf8360e2a2e59ca137022a578f27e91ea9c2b38
David Ghazi <davidaghazi@gmail.com>
2021-11-20 18:00:48 -0500

    merging with main branch

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -5,12 +5,9 @@ import { Link, useHistory } from "react-router-dom";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import HomeHeader from "../ReusableComponents/HomeHeader/Index";
 import ImageSlider from "./slideshow/ImageSlider";
-<<<<<<< HEAD
 import HomeHeadline from "../../components/HomePage/Headline/index";
-=======
 import SideBar from "../ReusableComponents/SidebarEvents/index";
 import Footer from "../ReusableComponents/Footer";
->>>>>>> main
 
 import Concerts from "../../api/Concerts";
 import Sports from "../../api/Sports";
@@ -41,13 +38,6 @@ function Home() {
   return (
     <>
       {/* <HomeHeader /> */}
-<<<<<<< HEAD
-      <HomeHeadline />
-      {/* <ImageSlider /> */}
-      {/* <Concerts />
-      <Sports />
-      <Arts /> */}
-=======
       {/* <ImageSlider /> */}
       {/* <SideBar
         src="https://cdn.pixabay.com/photo/2017/01/08/13/58/cube-1963036__340.jpg"
@@ -58,7 +48,6 @@ function Home() {
       <Sports />
       <Arts /> */}
       <Footer/>
->>>>>>> main
       {/* <div className={classes.test}>
         <h1>YoTixx</h1>
         <h1>Testing the automatic deploy again</h1>
